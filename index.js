236ad3981f4016f5f914665ec95969207cafb168
Marko Strba <mark3strba@gmail.com>
2024-01-12 01:27:15 +0100

    completed

---
+++ index.js
@@ -0,0 +1,31 @@
+const { parse } = require('csv-parse');
+const fs = require('fs')
+
+const results = []
+
+function isHabitalePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if( isHabitalePlanet(data)){
+            results.push(data);
+
+        }
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results.map((planet) => {
+            return planet['kepler_name'];
+        }))
+        console.log(`habitable planets: ${results.length}`);
+    });
